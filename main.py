aba1e124615da2c961d1dbfdcc13b293a92b157d
Arnav Thaker <arnav.thaker@gmail.com>
2023-01-10 19:37:20 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import scipy
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+import sklearn
+
+if __name__ == '__main__':
+    print("Sys version: ", sys.version)
+    print("Scipy version: ", scipy.__version__)
+    print("Numpy version: ", np.__version__)
+    print("matplotlib version: ", plt.__version__)
+    print("pandas version: ", pd.__version__)
+    print("sklearn version: ", sklearn.__version__)
+
