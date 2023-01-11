f3e5798453fee252a1e56d2a53dcba64b7c19f8a
Arnav Thaker <arnav.thaker@gmail.com>
2023-01-11 01:23:43 -0500

    Implemented methods to visualize the data using pandas and matplotlib tools. Some correlations can be seen, notably between petal_length and petal_width. Possibly ready to start considering a model/ algorithm?

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import sys
 import scipy
 import numpy as np
 import matplotlib as plt
+from matplotlib import pyplot
 import pandas as pd
+from pandas.plotting import scatter_matrix
 import sklearn
 
 class IrisFlowers():
@@ -20,9 +22,22 @@ class IrisFlowers():
         print("General statistics of dataset: \n", dataset.describe())
         print("Grouping by class (species): \n", dataset.groupby("species").size())
 
+    def univariate_visualizations(self, dataset):
+        dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+        pyplot.show()
+        dataset.hist()
+        pyplot.show()
+
+    def multivariate_visualizations(self, dataset):
+        scatter_matrix(dataset)
+        pyplot.show()
+
+
 if __name__ == '__main__':
     iris = IrisFlowers()
     iris_dataframe = iris.read_data()
     iris.observe_data(iris_dataframe)
+    iris.univariate_visualizations(iris_dataframe)
+    iris.multivariate_visualizations(iris_dataframe)
 
 
