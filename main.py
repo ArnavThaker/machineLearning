63e564d3b78ef4b14eaba8070e7ed876fb28e27c
Arnav Thaker <arnav.thaker@gmail.com>
2023-01-11 00:47:52 -0500

    Implemented methods to read and observe data from the Iris dataset csv. Everything is working and looks good so far. Nothing super serious done yet.

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ import matplotlib as plt
 import pandas as pd
 import sklearn
 
+class IrisFlowers():
+    def __init__(self):
+        pass
+
+    def read_data(self):
+        names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+        data = pd.read_csv('./iris.csv', names=names)
+        return data
+
+    def observe_data(self, dataset):
+        print("Dataset dimensions are: ", dataset.shape)
+        print("First 20 rows of data: \n", dataset.head(20))
+        print("General statistics of dataset: \n", dataset.describe())
+        print("Grouping by class (species): \n", dataset.groupby("species").size())
+
 if __name__ == '__main__':
-    print("Sys version: ", sys.version)
-    print("Scipy version: ", scipy.__version__)
-    print("Numpy version: ", np.__version__)
-    print("matplotlib version: ", plt.__version__)
-    print("pandas version: ", pd.__version__)
-    print("sklearn version: ", sklearn.__version__)
+    iris = IrisFlowers()
+    iris_dataframe = iris.read_data()
+    iris.observe_data(iris_dataframe)
 
 
